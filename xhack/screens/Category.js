d4994d30b37e9854a738ebe177d2f4b5bf084f48
kunalthedev <10kunalji@gmail.com>
2022-09-10 16:35:29 +0530

    made 2 screens

---
+++ xhack/screens/Category.js
@@ -0,0 +1,19 @@
+import {View, Text, ScrollView} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import CategoryCard, {localRestaurants} from '../components/CategoryCard';
+import BottomTabs from '../components/BottomTabs';
+
+const Category = ({navigation}) => {
+  const [restaurantData, setRestaurantData] = useState(localRestaurants);
+  return (
+    <SafeAreaView style={{backgroundColor: '#eee', flex: 1, padding: 15}}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <CategoryCard restaurantData={restaurantData} navigation={navigation} />
+      </ScrollView>
+      {/* <BottomTabs /> */}
+    </SafeAreaView>
+  );
+};
+
+export default Category;
